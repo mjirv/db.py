4bd931cdcc9f726f52dde55f36e165f3577cb24a
Michael Irvine <michael.j.irvine@gmail.com>
2022-03-11 21:40:21 -0500

    updates release script

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 python -m unittest discover db/tests/
 pandoc -f markdown -t rst README.md > README.rst
-python setup.py install sdist bdist_wheel $1
+python setup.py sdist bdist_wheel && python3 -m twine upload dist/*
